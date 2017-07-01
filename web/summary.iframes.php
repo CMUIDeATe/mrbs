7258c59dfc960648b300075ff304532f6d7614ee
Tim Parenti <tparenti@andrew.cmu.edu>
2017-07-01 16:48:25 -0400

    Fixes schedule iframes on test page.

--- web/summary.iframes.php
+++ web/summary.iframes.php
@@ -3,14 +3,14 @@
 </style>
 
 <table>
-<tr style="height: 90vh;">
+<tr>
 
 <?php
 
 foreach (array("A5", "A10", "A10A", "106B", "106C", "A29") as $room) {
   echo "<td style=\"width: 377px;\">";
     echo "<h2>".$room."</h2>";
-    echo "<iframe src=\"summary.display.php?room=".$room."\" style=\"height: 100%; border: 0;\"></iframe>";
+    echo "<iframe src=\"summary.display.php?room=".$room."\" style=\"height: 90vh; border: 0;\"></iframe>";
   echo "</td>";
 }
 
